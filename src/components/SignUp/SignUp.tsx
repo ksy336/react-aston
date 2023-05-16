7fd07335fc3b03eb601d5e0f9ab9e3231eb2ec05
ksy336 <ksuoxy773@gmail.com>
2023-05-16 17:00:58 +0400

    feat: add validation for login and password to signup page

--- src/components/SignUp/SignUp.tsx
+++ src/components/SignUp/SignUp.tsx
@@ -4,6 +4,9 @@ import Box from '@material-ui/core/Box';
 import TextField from '@material-ui/core/TextField';
 import Button from '../Button/Button';
 import { useForm } from 'react-hook-form';
+import"./Signup.scss";
+import { useNavigate } from 'react-router';
+
 type FormData = {
   login: string;
   password: string;
@@ -11,9 +14,12 @@ type FormData = {
 
 const SignUp = () => {
   const {register, handleSubmit, formState: {errors}, reset} = useForm<FormData>({mode: "onChange"});
+  const navigate = useNavigate();
+  // TODO: переместить в Сontext, реализовать сохранение в LS
   const [login, setLogin] = useState(""); // буду сохранять в LS + Context
+  const [password, setPassword] = useState("");
   const formSubmit = () => {
-
+    navigate("/signin");
   }
 
   return (
@@ -22,7 +28,7 @@ const SignUp = () => {
       <div className="login-container">
         <Container maxWidth="sm">
           <form className="form-signup" onSubmit={handleSubmit(formSubmit)}>
-            <Box mb={2}>
+            <Box mb={2} className="input-block">
               <TextField
               className="input-value"
               variant="outlined"
@@ -34,28 +40,35 @@ const SignUp = () => {
                 onChange: (e: React.ChangeEvent<HTMLInputElement>) => {setLogin(e.target.value)},
                 required: "Введите логин",
                 pattern: /^[a-zA-Z0-9]+$/i,
-                min: 3
 
               })
               }
                 error={!!errors.login}
               />
             </Box>
-            {errors?.login && <div>{errors?.login?.message}</div>}
-            <Box mb={2}>
+            {errors?.login && <div className="error-text">{errors?.login?.message}</div>}
+            <Box mb={2} className="input-block">
               <TextField
                 className="input-value"
                 variant="outlined"
                 label="Пароль"
                 fullWidth
                 autoFocus
+                type="password"
                 autoComplete="Пароль"
                 {...register("password", {
-                  onChange((e: React.ChangeEvent<HTML>) => {})
+                  onChange:((e: React.ChangeEvent<HTMLInputElement>) => {setPassword(e.target.value)}),
+                  required: "Введите пароль, минимум 6 символов",
+                  pattern: /^[A-Za-z]\w{6,14}$/,
+                  min: 8,
+                  max: 14
                 })}
               />
             </Box>
-            <Button type="submit">Зарегистрироваться</Button>
+            {errors?.password && <div className="error-text">{errors?.password.message}</div>}
+            <div className="button-register">
+              <Button type="submit">Зарегистрироваться</Button>
+            </div>
           </form>
         </Container>
       </div>
