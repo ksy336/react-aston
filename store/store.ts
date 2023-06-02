ca4fd0157a352dc6ae4c4a00b076a67c74ca08c3
ksy336 <ksuoxy773@gmail.com>
2023-06-02 15:33:57 +0400

    feat: add persist

--- store/store.ts
+++ store/store.ts
@@ -1,3 +1,14 @@
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { configureStore } from '@reduxjs/toolkit';
 import moviesReducer from "./movieSlice/movieSlice";
 import userReducer from "./userSlice/userSlice";
